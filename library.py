5578c5081d14a412c6be0924cafdc56bb10e3e26
Shyam Tandel <shyamtandel323@gmail.com>
2024-08-31 14:18:34 +0530

    Add exception if book is already exists

--- library.py
+++ library.py
@@ -1,9 +1,13 @@
 class Library:
     def __init__(self):
-        self.books = []
+        self.books = {}
 
-    def add_book(self, title, author):
-        book = {"title": title, "author": author}
-        self.books.append(book)
-        return book
-    
+    def add_book(self, bookid, title, author, publication_year):
+        if bookid in self.books:
+            raise Exception(f"Book with bookid {bookid} already exists.")
+        self.books[bookid] = {
+            "title": title,
+            "author": author,
+            "publication_year": publication_year,
+            "is_borrowed": False
+        }
--- test_library.py
+++ test_library.py
@@ -1,25 +1,15 @@
 import unittest
+from library import Library 
 
-# Sample Library class
-class Library:
-    def __init__(self):
-        self.books = []
-
-    def add_book(self, title, author):
-        book = {"title": title, "author": author}
-        self.books.append(book)
-        return book
-    
 # Test case for the add_book method
 class TestLibrary(unittest.TestCase):
     def setUp(self):
         self.library = Library()
 
-    def test_add_book_success(self):
-        book = self.library.add_book("2022", "Basic Maths")
-        self.assertEqual(len(self.library.books), 1)
-        self.assertEqual(book["title"], "2022")
-        self.assertEqual(book["author"], "Basic Maths")
+    def test_add_book(self):
+        self.library.add_book("1", "Basic Maths", "shyam", "2023")
+        self.assertIn("1", self.library.books)
+        self.assertEqual(self.library.books["1"]["title"], "Basic Maths")
 
 if __name__ == "__main__":
     unittest.main()
